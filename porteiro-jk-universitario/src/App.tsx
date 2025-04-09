549dd78a879e1c032e5814cc3ef0f44dae819de7
JkUniversitario421 <jkuniversitario421@gmail.com>
2025-04-09 13:42:44 +0000

    1

---
+++ porteiro-jk-universitario/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-bold text-green-600 mb-4">
+        Porteiro JK Universitário
+      </h1>
+      <p className="text-gray-700">
+        Vamos começar configurando a interface e integrando com Google Sheets.
+      </p>
+    </div>
+  );
+}
+
+export default App;
